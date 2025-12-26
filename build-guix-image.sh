f313a59efb81f7c7ecf53419c6db4e5c5961b5a7
user2595 <42352562+user2595@users.noreply.github.com>
2025-12-26 09:03:12 +0100

    Remove old guix-container-tools.tar.xz before copy
    Remove the old tar.xz file before copying the new one.

--- build-guix-image.sh
+++ build-guix-image.sh
@@ -14,6 +14,7 @@ else
   guix pack "${params[@]}"
 fi
 TARGET=$(readlink -f /workdir/guix-container-tools.tar.xz)
+rm /workdir/guix-container-tools.tar.xz
 cp "$TARGET" /workdir/guix-container-tools.tar.xz
 echo "✓ Image created: /workdir/guix-container-tools.tar.xz"
 ls -lh /workdir/guix-container-tools.tar.xz
